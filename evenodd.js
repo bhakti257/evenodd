0fd9932b87531fe727b6294bcfd0932f435d1cbd
bhakti257 <bhakti.rajai.2002@gmail.com>
2024-12-26 16:04:51 +0530

    Initial commit

---
+++ evenodd.js
@@ -0,0 +1,6 @@
+let numbers = [1, 2, 3, 4, 5, 6];
+let evenNumbers = numbers.filter(num => num % 2 === 0);
+let oddNumbers = numbers.filter(num => num % 2 !== 0);
+
+console.log("Even Numbers:", evenNumbers);
+console.log("Odd Numbers:", oddNumbers);
