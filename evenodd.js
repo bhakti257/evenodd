22c9d9b63b2cb7fe61a12702779952f164c19116
bhakti257 <bhakti.rajai.2002@gmail.com>
2024-12-26 16:05:56 +0530

    array modified

--- evenodd.js
+++ evenodd.js
@@ -1,4 +1,4 @@
-let numbers = [1, 2, 3, 4, 5, 6];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let evenNumbers = numbers.filter(num => num % 2 === 0);
 let oddNumbers = numbers.filter(num => num % 2 !== 0);
 
