3ba4b0b85e82d6794441b115da635934218a1870
bhakti257 <bhakti.rajai.2002@gmail.com>
2024-12-26 16:07:29 +0530

    comment added

--- evenodd.js
+++ evenodd.js
@@ -1,4 +1,5 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//filter function filters according to condition
 let evenNumbers = numbers.filter(num => num % 2 === 0);
 let oddNumbers = numbers.filter(num => num % 2 !== 0);
 
